2bd0539ef37d157a39c97440d30ca8e83e29893c
zanit-solution <zanltd.bd@gmail.com>
2024-12-20 22:55:13 +0600

    added new problem

--- day-6/palindrome-3.cpp
+++ day-6/palindrome-3.cpp
@@ -1,1 +1,27 @@
- 
+ #include <bits/stdc++.h>
+using namespace std;
+#define testCase while(tt--)
+#define ll long long int
+#define loop(i, start, end) for(int i = (start); i < (end); i++)
+#define nl '\n'
+#define cyes cout << "YES\n";
+#define cno cout << "NO\n";
+void solve(){
+  int n;
+  cin>>n;
+  string st ="abc";
+  string vl = "";
+  loop(i,0,n){
+    vl+=st[i%3];
+  }
+  cout<<vl;
+        
+}
+int main() 
+{
+    int tt;
+   
+        solve(); 
+    
+    return 0;
+}
